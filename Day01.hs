1b40964110b534bed9b2899aab8c47e43be8437d
Konstantin Budikhin <me@bood.su>
2019-12-10 02:31:39 +0300

    Add Day01 part 2

--- Day01.hs
+++ Day01.hs
@@ -10,15 +10,25 @@ import           Data.Char
 parse :: String -> [Int]
 parse = map digitToInt
 
-calc :: [Int] -> Int
-calc is = go 0 is
+calc1 :: [Int] -> Int
+calc1 is = go 0 is
  where
   go r (x : y : xs) | x == y    = go (r + x) (y : xs)
                     | otherwise = go r (y : xs)
   go r [x] | x == head is = r + x
   go r []                 = r
 
+calc2 :: [Int] -> Int
+calc2 [] = 0
+calc2 is = sum $ take l $ zipWith f cycled shifted
+ where
+  cycled  = cycle is
+  shifted = drop (l `div` 2) cycled
+  l       = length is
+  f i j = if i == j then i else 0
+
 main :: IO ()
 main = do
-  res <- calc . parse <$> getLine
-  print res
+  is <- parse <$> getLine
+  print $ calc1 is
+  print $ calc2 is
